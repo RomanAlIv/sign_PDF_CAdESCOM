e995705a31d7593fb3589de5fab22f6da0a4cc4a
RomanAlIv <133589565+RomanAlIv@users.noreply.github.com>
2023-09-25 10:50:25 +0300

    Update core.py

--- core.py
+++ core.py
@@ -1,7 +1,15 @@
 from base64 import b64encode
 from typing import Union
 
-import pycades
+import win32com.client as win32
+
+from cadescom_const import CADESCOM_CONTAINER_STORE, CAPICOM_MY_STORE, CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED, \
+    CADESCOM_CADES_BES, CADESCOM_BASE64_TO_BINARY, CADESCOM_HASH_ALGORITHM_CP_GOST_3411_2012_256
+
+import pythoncom
+import pywintypes
+#Сразу перед инициализацией DCOM в run()
+
 
 
 def certificate_info(cert):
@@ -35,27 +43,30 @@ def certificate_info(cert):
 
 def parse_detail(row):
     if row:
+        row1 = row.replace(', д.', ' д.').replace(', стр.', ' стр.').replace(', эт.', ' эт.').replace(', пом.',' пом.').replace(', ком.', ' ком.').replace(', ПОМЕЩ', ' ПОМЕЩ').replace(', КОМ', ' КОМ')
         detail = dict(
             key_val.split('=')
-            for key_val in row.split(',')
+            for key_val in row1.split(',')
         )
-        detail['row'] = row
+        detail['row'] = row1
         return detail
 
 
 def certificates_store():
-    store = pycades.Store()
+    pythoncom.CoInitializeEx(0)
+    store = win32.Dispatch("CAdESCOM.Store")
     store.Open(
-        pycades.CADESCOM_CONTAINER_STORE,
-        pycades.CAPICOM_MY_STORE,
-        pycades.CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED
+        CADESCOM_CONTAINER_STORE,
+        CAPICOM_MY_STORE,
+        CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED
     )
     return store.Certificates
 
 
 def get_signer(cert, pin):
     """Формирование подписи."""
-    signer = pycades.Signer()
+    pythoncom.CoInitializeEx(0)
+    signer = win32.Dispatch("CAdESCOM.CPSigner")
     signer.Certificate = cert
     signer.CheckCertificate = True
     signer.KeyPin = pin
@@ -64,16 +75,18 @@ def get_signer(cert, pin):
 
 def get_signature(file, signer):
     """Подпись файла."""
-    signed_data = pycades.SignedData()
+    pythoncom.CoInitializeEx(0)
+    signed_data = win32.Dispatch("CAdESCOM.CadesSignedData")
     signed_data.Content = b64encode(file).decode()
-    return signed_data.SignCades(signer, pycades.CADESCOM_CADES_BES)
+    return signed_data.SignCades(signer, CADESCOM_CADES_BES)
 
 
 def get_unsigned(signature: bytes) -> str:
     """Разподписать файл."""
-    unsigned_data = pycades.SignedData()
+    pythoncom.CoInitializeEx(0)
+    unsigned_data = win32.Dispatch("CAdESCOM.CadesSignedData")
     signature = b64encode(signature).decode()
-    unsigned_data.VerifyCades(signature, pycades.CADESCOM_CADES_BES)
+    unsigned_data.VerifyCades(signature, CADESCOM_CADES_BES)
     return unsigned_data.Content
 
 
@@ -82,10 +95,11 @@ def gost_hash(data: Union[str, bytes, bytearray], encoding="utf-8") -> str:
     if isinstance(data, str):
         data = bytes(data.encode(encoding))
 
-    hashed_data = pycades.HashedData()
-    hashed_data.DataEncoding = pycades.CADESCOM_BASE64_TO_BINARY
+    pythoncom.CoInitializeEx(0)
+    hashed_data = win32.Dispatch("CAdESCOM.HashedData")
+    hashed_data.DataEncoding = CADESCOM_BASE64_TO_BINARY
     hashed_data.Algorithm = (
-        pycades.CADESCOM_HASH_ALGORITHM_CP_GOST_3411_2012_256
+        CADESCOM_HASH_ALGORITHM_CP_GOST_3411_2012_256
     )
     hashed_data.Hash(b64encode(data).decode())
     byte_hash = bytes.fromhex(hashed_data.Value)
